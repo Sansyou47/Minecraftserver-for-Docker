7d3725bfa98f501d5b37226598646322ca94716f
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-11-02 23:39:30 +0900

    サーバーコントロールコマンドの追加

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'hogehoge'
+    return render_template('index.html')
 
 @app.route('/gm')
 def gm():
@@ -15,10 +15,21 @@ def gm():
 
 @app.route('/stop')
 def sb():
-    cmd=["mcrcon", "-H", "mcserver", "-p", "test", "/give", "Sansyou47", "stone","5"]
+    cmd=["mcrcon", "-H", "mcserver", "-p", "test", "give Sansyou47 stone 5"]
     subprocess.run(cmd)
     return 'hoge'
 
+@app.route('/action', methods=['POST'])
+def action():
+    do_action=int(request.form.get('ctrl_type'))
+    if do_action == 0:
+        os.system('mcrcon -H mcserver -p test stop')
+    elif do_action == 1:
+        os.system('mcrcon -H mcserver -p test "gamemode survival @a"')
+    elif do_action ==2:
+        os.system('mcrcon -H mcserver -p test help')
+    return redirect('/')
+    
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
-    7
+    
