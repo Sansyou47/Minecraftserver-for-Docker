b8be2b257c7ecef9a527d54da3eca7547bef9094
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-11-10 23:51:15 +0900

    コマンドを登録

--- db/init.sql
+++ db/init.sql
@@ -1,8 +1,28 @@
 create table command(
     number  int auto_increment primary key,
     name    varchar(10) not null,
-    level   int not null
+    argument   int not null
 )
+
+insert into command(name,argument) values('help', 0)
+insert into command(name,argument) values('kick', 1)
+insert into command(name,argument) values('ban', 1)
+insert into command(name,argument) values('pardon', 1)
+insert into command(name,argument) values('ban-ip', 1)
+insert into command(name,argument) values('pardon-ip', 1)
+insert into command(name,argument) values('op', 1)
+insert into command(name,argument) values('deop', 1)
+insert into command(name,argument) values('tp', 1)
+insert into command(name,argument) values('give', 1)
+insert into command(name,argument) values('stop', 0)
+insert into command(name,argument) values('save-all', 0)
+insert into command(name,argument) values('save-off', 0)
+insert into command(name,argument) values('save-on', 0)
+insert into command(name,argument) values('list', 0)
+insert into command(name,argument) values('say', 1)
+insert into command(name,argument) values('whitelist', 1)
+insert into command(name,argument) values('time', 1)
+insert into command(name,argument) values('gamemode', 1)
 
 drop table if exists target;
 
