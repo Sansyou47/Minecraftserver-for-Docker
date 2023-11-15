d28569ad2e9f9dcb3c8f0ff53dd3c55291cbe089
Kazuhiro Yomesaka <yomesaka.ikkousen@gmail.com>
2023-11-15 18:53:56 +0900

    データベースの初期化設定

--- db/init.sql
+++ db/init.sql
@@ -24,24 +24,24 @@ insert into command(name,argument) values('whitelist', 1)
 insert into command(name,argument) values('time', 1)
 insert into command(name,argument) values('gamemode', 1)
 
-drop table if exists target;
+-- drop table if exists target;
 
-create table target(
-    number  int auto_increment primary key,
-    name    varchar(20) not null,
-    expl    varchar(100)
-)
+-- create table target(
+--     number  int auto_increment primary key,
+--     name    varchar(20) not null,
+--     expl    varchar(100)
+-- )
 
-insert into target(name,expl) values('@a', 'all player');
-insert into target(name) values('@e', '');
-insert into target(name) values('@p', 'nearest player');
-insert into target(name) values('@r', 'random player');
+-- insert into target(name) values('@a', 'all player');
+-- insert into target(name) values('@e', '');
+-- insert into target(name) values('@p', 'nearest player');
+-- insert into target(name) values('@r', 'random player');
 
 
-drop table if exists items;
+-- drop table if exists items;
 
-create table items(
-    number  int auto_increment primary key,
-    name    varchar(20) not null,
-    expl    varchar(100)
-)
+-- create table items(
+--     number  int auto_increment primary key,
+--     name    varchar(20) not null,
+--     expl    varchar(100)
+-- )
